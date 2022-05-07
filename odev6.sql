a8b09adbef96a5193f2c198ab623a3c6fe988e1f
Furkan Aydın <aydinffurkan@gmail.com>
2022-05-07 17:24:27 +0300

    aggregate fonksiyonlar

---
+++ odev6.sql
@@ -0,0 +1,8 @@
+-- avg:ortalama almamızı sağlar
+--max: sütunda ki en yüksek değeri gösterir
+--min:sütünda ki en düşük değeri gösterir.
+
+--SELECT AVG(rental_rate) FROM film
+--SELECT COUNT(*) FROM film WHERE title LIKE('C%');
+--SELECT MAX(length) FROM film WHERE rental_rate=0.99 
+--SELECT COUNT(DISTINCT(replacement_cost)) FROM film WHERE length>150
