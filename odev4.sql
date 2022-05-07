b693858f0e589346b4d2dd404fb7a85fdff3e08f
Furkan Aydın <aydinffurkan@gmail.com>
2022-05-07 16:04:50 +0300

    DISTINCT VE COUNT KAVRAMLARI

---
+++ odev4.sql
@@ -0,0 +1,9 @@
+-- DISTINCK ifadesi bir sütundaki birbirinden farklı verileri liste halinde sıralamaya yarar
+--COUNT İFADESİ SAYAÇ GÖREVİ GÖRÜR. SÜTUNDAKİ TOPLAM VERİ SAYISINI VERİR.
+
+--SELECT DISTINCT replacement_cost FROM film;
+--SELECT COUNT(DISTINCT replacement_cost) from film;
+--SELECT COUNT(*) FROM film WHERE title LIKE('T%') AND rating='G'
+--SELECT COUNT(*) from country WHERE country LIKE('_____')
+--SELECT COUNT(*) from city WHERE city ILIKE('%R')
+
